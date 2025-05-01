263cb604511519004dd691d44b59adb0ae41ebb3
CvaTricks <68840422+cvatricks@users.noreply.github.com>
2025-05-01 23:23:50 +0530

    Add files via upload

---
+++ config.py
@@ -0,0 +1,15 @@
+import pymongo
+
+class Config(object):
+
+    # Mongodb
+    MONGODB_URI = "mongodb+srv://sivaprakasamulaganathan:7yVnXJwsk8M1p1Af@cluster0.v6vqw1o.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0"
+    myclient = pymongo.MongoClient(MONGODB_URI)
+    mydb = myclient["mydatabase"]
+    users = mydb["users"]
+    sessions = mydb["sessions"]
+    workdata = mydb["data"]
+
+    # Connection
+    HOST = "192.168.0.102"
+    PORT = "5000"
---
+++ server.py
@@ -0,0 +1,99 @@
+import datetime
+from datetime import datetime
+from config import Config
+from flask import Flask, request, jsonify, render_template
+from werkzeug.security import check_password_hash
+
+app = Flask(__name__)
+
+# MongoDB Connection
+users_collection = Config.users  # Collection where users are stored
+active_sessions = Config.sessions # Collection where sessions are stored
+workdbdata = Config.workdata # traceablity data
+
+@app.route("/", methods=["GET"])
+def indexPage():
+    return render_template("/index.html", HOST=Config.HOST, PORT=Config.PORT)
+
+@app.route("/login", methods=["POST"])
+def login():
+    data = request.json
+    login_id = data.get("loginId")
+    password = data.get("password")
+    # Find user in MongoDB
+    user = users_collection.find_one({"login_id": login_id})
+
+    if user and (user["password"] == password):
+        login_id = login_id
+        login_time = str(datetime.now())
+        session = {
+            "login_id": login_id,
+            "login_time": login_time
+        }
+        sessions = active_sessions.insert_one(session)
+        return jsonify({"success": True, "message": "Login successful"})
+    else:
+        return jsonify({"success": False, "message": "Invalid credentials"}), 401
+
+@app.route("/logout", methods=["POST"])
+def logout():
+    data = request.json
+    login_id = data.get("loginId")
+    # Find user in MongoDB
+    user = users_collection.find_one({"login_id": login_id})
+    if user:
+        login_id = login_id
+        session = {
+            "login_id": login_id
+        }
+        sessions = active_sessions.delete_one(session)
+        return jsonify({"success": True, "message": "Logout successful"})
+    else:
+        return jsonify({"success": False, "message": "Logout failed"}), 401
+
+@app.route("/dashboard", methods=["GET"])
+def dashboardPage():
+    return render_template("/dashboard.html", HOST=Config.HOST, PORT=Config.PORT)
+
+
+@app.route('/workstart', methods=['GET', 'POST'])
+def workstart():
+    return render_template("/scancode.html", HOST=Config.HOST, PORT=Config.PORT)
+
+@app.route('/updatedata', methods=['GET', 'POST'])
+def updatedata():
+    data = request.json
+    decodedText = data.get("decodedText")
+    user = data.get("user")
+    workdata = {
+        "process_name" : user,
+        "work" : "start",
+        "datetime" : datetime.now(),
+        "jobid" : decodedText
+    }
+    workdataupdated = workdbdata.insert_one(workdata)
+    if workdataupdated:
+        return jsonify({"success": True, "message": f"Work started by {user}"})
+    else:
+        return jsonify({"success": False, "message": "Work not able to start!"})
+
+@app.route('/reports', methods=['GET', 'POST'])
+def reports():
+    data = request.json
+    user = data.get("user")
+    report = "<tr><td>Process</td><td>Work</td><td>DateTime</td><td>Job ID</td></tr>"
+    workdata = {
+        "process_name" : "admin"
+    }
+    workdataupdated = workdbdata.find(workdata)
+    for x in workdataupdated:
+        itsdata = f'<tr><td>{x.get("process_name")}</td><td>{x.get("work")}</td><td>{x.get("datetime")}</td><td>{x.get("jobid")}</td></tr>'
+        report = report + itsdata
+    if report != "<tr><td>Process</td><td>Work</td><td>DateTime</td><td>Job ID</td></tr>":
+        return jsonify({"success": True, "message": report})
+    else:
+        return jsonify({"success": False, "message": "No records Found!"})
+
+if __name__ == "__main__":
+    #app.run(debug=True)
+    app.run(host=Config.HOST, port=Config.PORT)
