733df7974e5ce7665544d78a40bdef85b4c07c97
CvaTricks <68840422+cvatricks@users.noreply.github.com>
2025-11-07 14:08:08 +0530

    Update config.py
    Changed to Santhosh db

--- config.py
+++ config.py
@@ -3,7 +3,8 @@ import pymongo
 class Config(object):
 
     # Mongodb
-    MONGODB_URI = "mongodb+srv://sivaprakasamulaganathan:7yVnXJwsk8M1p1Af@cluster0.v6vqw1o.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0"
+    MONGODB_URI = "mongodb+srv://sandytpm5959_db_user:ugTrbPHLOhpGE79F@tracebility.y637mls.mongodb.net/?retryWrites=true&w=majority&appName=Tracebility"
+    # MONGODB_URI = "mongodb+srv://sivaprakasamulaganathan:7yVnXJwsk8M1p1Af@cluster0.v6vqw1o.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0"
     myclient = pymongo.MongoClient(MONGODB_URI)
     mydb = myclient["mydatabase"]
     users = mydb["users"]
@@ -15,3 +16,4 @@ class Config(object):
     HOST = "traceability-sdzt.onrender.com"
     IP = "0.0.0.0"
     PORT = "80"
+
